6e43e434854c9a6a121b72acc73b32e0e71f3f6f
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-01-27 03:01:53 +0900

    REFACTOR : URL변수들을 저장할 함수 제작
    String형 URL변수들을 저장하기 위한 구조체를 저장하기 위한 헤더파일 User URL.h를 제작했다.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Header File/Sieve Information.h"
 #include "Header File/Base64.h"
 #include "Header File/HTTP Request.h"
+#include "Header File/User URL.h"
 
 #define Information_Size 6
 
@@ -11,21 +12,18 @@ using namespace std;
 
 int main(void) {
     User_Information User;
+    User_URL User_URL;
     string Information[Information_Size];
     Information_Input(Information_Size, Information);
     Sieve_Information(Information_Size, Information, User);
 
     string HEADER = base64_encode(User.UserName + ":" + User.Token);
-    string User_URL = "https://api.github.com/user";
-    string User_Target_Followers_URL = "https://api.github.com/users/" + User.UserTarget + "/followers";
-    string User_Following_URL = "https://api.github.com/user/following/";
-    string User_Page_Request_Str = HTTP_Request_GET(User_URL, HEADER);
+    string User_Page_Request_Str = HTTP_Request_GET(User_URL.User_API_URL, HEADER);
 
     if (User_Page_Request_Str.length() < 1000) {
         cout << "ERROR";
     } else {
         cout << "Connecting OK!";
-
     }
     return 0;
 }
