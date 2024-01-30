dea357362afd712f6f67870d3435c89da7adbe47
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-01-31 04:32:58 +0900

    REFACTOR : 코드구조 단순화
    실행파일들은 src, 헤더파일들은 include에 정리하였다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include "Header File/Information Input.h"
-#include "Header File/Sieve Information.h"
-#include "Header File/Base64.h"
-#include "Header File/HTTP Request.h"
-#include "Header File/User URL.h"
-
+#include "../Header File/Information Input.h"
+#include "../Header File/Sieve Information.h"
+#include "../Header File/Base64.h"
+#include "../Header File/HTTP Request.h"
+#include "../Header File/User URL.h"
 #define Information_Size 6
 
 using namespace std;
